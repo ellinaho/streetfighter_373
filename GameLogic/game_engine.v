6f70cc12a50e6ce9cd2c39fe1631204e2acebcea
JCPeanu <62139419+JCPeanu@users.noreply.github.com>
2026-04-06 20:00:55 -0400

    New updates to code

--- GameLogic/game_engine.v
+++ GameLogic/game_engine.v
@@ -9,15 +9,50 @@ module game_engine(
     input  wire [6:0] p1_hp,
     input  wire [6:0] p2_hp,
     // To the rest of the System
-    output reg [1:0]  game_state_out, // 00: STARTUP, 01: PLAY, 10: GAMEOVER
-    output reg        round_reset     // Pulses high to reset Player modules
+    output reg [2:0]  game_state_out, // 00: STARTUP, 01: PLAY, 10: GAMEOVER
+    output reg        round_reset,     // Pulses high to reset Player modules
+    output wire [6:0] time_remaining,
+    output wire match_over,
+    output wire p1_winner,
+    output wire p2_winner
 );
+
+    assign match_over = (current_state == GAMEOVER);
+    assign p1_winner = match_over && ((p2_hp == 0) || (p1_hp > p2_hp));
+    assign p2_winner = match_over && ((p1_hp == 0) || (p2_hp > p1_hp));
 
      // State definitions and transitions
     localparam STARTUP = 3'd0, PLAY = 3'd1, GAMEOVER = 3'd2;
-    reg [2:0] current_state;
+    reg [2:0] current_state, next_state;
     // typedef enum reg [1:0] {STARTUP = 2'b00, PLAY = 2'b01, GAMEOVER = 2'b10} state_t;
     // state_t current_state, next_state;
+
+    reg [25:0] clk_counter;
+    wire one_second_pulse = (clk_counter == 26'd49_999_999);
+
+    always @(posedge clk) begin
+        if (rst || current_state != PLAY) begin
+            clk_counter <= 0;
+        end
+        else if (clk_counter == 26'd49_999_999) begin
+            clk_counter <= 0;
+        end
+        else begin
+            clk_counter <= clk_counter + 1;
+        end
+    end
+
+    reg [6:0] round_timer; // 7 bits can hold up to 127
+    assign time_remaining = round_timer;
+    always @(posedge clk) begin
+        if (rst || current_state == STARTUP) begin
+            round_timer <= 7'd96; // Reset time to 99
+        end else if (current_state == PLAY && one_second_pulse) begin
+            if (round_timer > 0) begin
+                round_timer <= round_timer - 1;
+            end
+        end
+    end
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
@@ -36,7 +71,7 @@ module game_engine(
             end
 
             PLAY: begin
-                if (p1_hp == 0 || p2_hp == 0) next_state = GAMEOVER;
+                if (p1_hp == 0 || p2_hp == 0 || round_timer == 0) next_state = GAMEOVER;
             end
 
             GAMEOVER: begin
