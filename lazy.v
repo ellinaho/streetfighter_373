2bce7e53ca0fcc2097a61c1a5ec4b52bcaa39c59
ellinaho <ellinaho@gmail.com>
2026-04-09 20:59:32 -0400

    Update lazy.v

--- lazy.v
+++ lazy.v
@@ -1,8 +1,21 @@
-    wire       p1_jump_cmd, p2_jump_cmd;
-    wire       p1_punch_cmd, p2_punch_cmd;
-    wire [3:0] p1_punch_val, p2_punch_val; // Now it can hold all 4 bits!
-    wire       p1_charge_cmd, p2_charge_cmd;
-    wire [1:0] p1_move_cmd, p2_move_cmd;   // Now it can hold both bits!
+else if (move_cmd == 2'b01 && (my_hurtbox_right + H_speed < MAX_WIDTH)) begin
+    player_state <= WALK;
+end
+else if (move_cmd == 2'b10 && (my_hurtbox_left > H_speed && my_hurtbox_left < 512)) begin
+    player_state <= WALK;
+end
 
-    wire       p1_start_button, p2_start_button;
-    wire       rst_button;
+
+// Going Left
+    else if (move_cmd == 2'b10) begin
+        // 1. First check if we have hit the wall (WITH UNDERFLOW PROTECTION)
+        if (my_hurtbox_left > H_speed && my_hurtbox_left < 512) begin
+
+WALK: begin
+    //update coordinates 
+    if (p1_dir == RIGHT) begin 
+        if ((p1_x + SPRITE_W - 15) < GAME_W) p1_x <= p1_x + WALK_SPEED;
+    end else begin 
+        if ((p1_x + 15) > 0 && (p1_x + 15) < 512) p1_x <= p1_x - WALK_SPEED;
+    end
+    // ... (leave animation timer alone)
